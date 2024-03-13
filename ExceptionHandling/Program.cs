8b3de284c3218f763c3760c15ac19bef90a5937d
Oğuzhan Söğünmez <oguzhann016s@gmail.com>
2024-03-13 15:05:02 +0300

    ExceptionHandling added.

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,30 @@
+﻿try
+{
+    while (true)
+    {
+        var key = Console.ReadKey();
+        if (key.Key == ConsoleKey.R)
+            throw new CustomException("Laylaylom galiba sana göre sevmeler...")
+            {
+                MyProperty = 5
+            };
+        else
+            Console.WriteLine(key.Key);
+    }
+}
+catch (CustomException ex)
+{
+
+}
+
+class CustomException : Exception
+{
+    public int MyProperty { get; set; }
+    public CustomException() : base("")
+    {
+    }
+
+    public CustomException(string? message) : base(message)
+    {
+    }
+}
---
+++ ProgrammaticInstance/Program.cs
@@ -0,0 +1,19 @@
+﻿#region Acticator Sınıfı ile Nesne Oluşturma
+Activator.CreateInstance(typeof(MyClass));
+#endregion
+#region DynamicObject Sınıfı ile Nesne Oluşturma
+
+dynamic x = new MyClass();
+x.A();
+
+#endregion
+#region Dynamic Keyword'ü ile Nesne Oluşturma
+#endregion
+
+class MyClass
+{
+    public MyClass()
+    {
+        Console.WriteLine($"{nameof(MyClass)} instance created.");
+    }
+}
