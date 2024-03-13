0b0b93829e04e2d2cfd2dc1cb7ef40edfdece93d
Oğuzhan Söğünmez <oguzhann016s@gmail.com>
2024-03-13 15:15:53 +0300

    ProgrammaticInstance added.

--- ProgrammaticInstance/Program.cs
+++ ProgrammaticInstance/Program.cs
@@ -1,19 +1,41 @@
-﻿#region Acticator Sınıfı ile Nesne Oluşturma
+﻿using System.Dynamic;
+
+#region Acticator Sınıfı ile Nesne Oluşturma
 Activator.CreateInstance(typeof(MyClass));
 #endregion
+
 #region DynamicObject Sınıfı ile Nesne Oluşturma
+dynamic instance = new MyClass();
+instance.DynamicProperty1 = "123";
+instance.DynamicProperty2 = 123;
 
-dynamic x = new MyClass();
-x.A();
-
+MyClass _instance = (MyClass)instance;
+//_instance.WriteMembers(); derleme zamanı hatası.
 #endregion
+
 #region Dynamic Keyword'ü ile Nesne Oluşturma
+dynamic instance2 = new ExpandoObject();
+instance2.DynamicProperty1 = "123";
+instance2.DynamicProperty2 = 123;
 #endregion
 
-class MyClass
+class MyClass : DynamicObject
 {
     public MyClass()
     {
         Console.WriteLine($"{nameof(MyClass)} instance created.");
+    }
+
+    readonly private Dictionary<string, object> properties = new();
+
+    public override bool TrySetMember(SetMemberBinder binder, object? value)
+    {
+        properties.Add(binder.Name, value);
+        return true;
+    }
+
+    public override bool TryGetMember(GetMemberBinder binder, out object? result)
+    {
+        return properties.TryGetValue(binder.Name, out result);
     }
 }
